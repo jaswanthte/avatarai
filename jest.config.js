3dab1608441f46d18f8d2447ddac6f935040a71d
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-22 17:58:16 +0530

    updt2

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    transformIgnorePatterns: [
+      "/node_modules/(?!(microsoft-cognitiveservices-speech-sdk)/)"
+    ],
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    },
+    moduleNameMapper: {
+      "\\.css$": "identity-obj-proxy"
+    }
+  };
+  
