26e29730175d298835274f964c587e6ad9af0f98
jaswanthte <jaswanth.sabbineni@techenhance.com>
2024-07-22 17:16:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { Avatar } from './components/Avatar';
 
 function App() {
   return (
     <div className="App">
-        <Avatar/>
+      <Avatar />
     </div>
   );
 }
